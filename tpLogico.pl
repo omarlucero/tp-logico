d71ec19281844b332a057599de1668b4ef351a35
Unknown <omarlucero1994@gmail.com>
2017-05-15 16:11:01 -0300

    se agrego a la base de conocimiento las propiedades con sus caracteristicas

---
+++ tpLogico.pl
@@ -0,0 +1,20 @@
+esPropiedad(tinsmithCircle1774).
+esPropiedad(avMoreno708).
+esPropiedad(avSiempreViva742).
+esPropiedad(calleFalsa123).
+
+ambientes(tinsmithCircle1774, 3).
+ambientes(avMoreno708, 7).
+ambientes(avSiempreViva742, 4).
+ambientes(calleFalsa123, 3).
+
+cuesta(tinsmithCircle1774, 700).
+cuesta(avMoreno708, 2000).
+cuesta(avSiempreViva742, 1000).
+cuesta(calleFalsa123, 200).
+
+tieneJardin(tinsmithCircle1774).
+tieneJardin(avMoreno708).
+tieneJardin(avSiempreViva742).
+
+tienePiscina(avMoreno708, 30).
