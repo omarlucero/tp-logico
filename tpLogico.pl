dbca437605211c88dd16c35cf45f0e8a8ff4cfd6
Unknown <omarlucero1994@gmail.com>
2017-06-20 00:42:00 -0300

    corregido
    corregi lo marcado por el profesor, lo que concluiria por el momento la segunda entrega

--- tpLogico.pl
+++ tpLogico.pl
@@ -7,13 +7,16 @@ esPropiedad(UnaPropiedad):- cuesta(UnaPropiedad, _).
 
 tiene(tinsmithCircle1774, ambientes(3)).
 tiene(tinsmithCircle1774, jardin).
+tiene(tinsmithCircle1774, instalaciones([extractorDeAire, aireAcondicionado, calefaccionAGas])).
 
 tiene(avMoreno708, ambientes(7)).
 tiene(avMoreno708, piscina(30)).
 tiene(avMoreno708, jardin).
+tiene(avMoreno708, instalaciones([aireAcondicionado, extractorDeAire, calefaccionPorLozaRadiante, vidriosDobles])).
 
 tiene(avSiempreViva742, ambientes(4)).
 tiene(avSiempreViva742, jardin).
+tiene(avSiempreViva742, instalaciones([calefaccionAGas])).
 
 tiene(calleFalsa123, ambientes(3)).
 
@@ -27,30 +30,31 @@ loQueQuiere(carlos, jardin).
 loQueQuiere(carlos, ambientes(3)).
 
 loQueQuiere(ana, piscina(100)).
+loQueQuiere(ana, instalaciones([aireAcondicionado, vidriosDobles])).
 
 loQueQuiere(maria, ambientes(2)).
 loQueQuiere(maria, piscina(15)).
 
 loQueQuiere(pedro, Comodidad):- loQueQuiere(maria, Comodidad).
+loQueQuiere(pedro, instalaciones([vidriosDobles, calefaccionPorLozaRadiante])).
 
 loQueQuiere(chameleon, Comodidad):- persona(Cliente), Cliente \= chameleon, loQueQuiere(Cliente, Comodidad).
 
 cumpleCondicion(Propiedad, Comodidad):-
-  esPropiedad(Propiedad),
   loQueQuiere(_, Comodidad),
   tiene(Propiedad, Comodidad).
 
 cumpleCondicion(Propiedad, ambientes(AmbientesDeseados)):-
-  esPropiedad(Propiedad),
   loQueQuiere(_, ambientes(AmbientesDeseados)),
   tiene(Propiedad, ambientes(AmbientesDePropiedad)),
   AmbientesDePropiedad >= AmbientesDeseados.
 
 cumpleCondicion(Propiedad, piscina(MetrosDeseados)):-
- esPropiedad(Propiedad),
  loQueQuiere(_, piscina(MetrosDeseados)),
  tiene(Propiedad, piscina(MetrosDePiscinaReales)),
  MetrosDePiscinaReales >= MetrosDeseados.
+
+comodidadIncumplida(Comodidad):- loQueQuiere(_, Comodidad), not(cumpleCondicion(_, Comodidad)).
 
 /*parte 2 */
 
@@ -59,7 +63,44 @@ cumpleTodo(Propiedad, Persona):-
   esPropiedad(Propiedad),
   forall(loQueQuiere(Persona, Comodidad), cumpleCondicion(Propiedad, Comodidad)).
 
+mejorOpcionVersionConForall(Persona, PropiedadMasBarata):-
+  persona(Persona),
+  cumpleTodo(PropiedadMasBarata, Persona),
+  cuesta(PropiedadMasBarata, PrecioMasBarato),
+  forall(cumpleTodo(OtraPropiedad, Persona), (cuesta(OtraPropiedad, PrecioMasCaro), PrecioMasCaro >= PrecioMasBarato)).
 
+mejorOpcionVersionConNot(Persona, PropiedadMasBarata):-
+  persona(Persona),
+  cumpleTodo(PropiedadMasBarata, Persona),
+  cuesta(PropiedadMasBarata, PrecioMasBarato),
+  not((cumpleTodo(PropiedadMasCara, Persona), cuesta(PropiedadMasCara, PrecioMasCaro), PrecioMasCaro < PrecioMasBarato)).
+
+efectividad(Puntaje):-
+  personasSatisfechas(PersonasSatisfechas),
+  length(PersonasSatisfechas, Cantidad),
+  totalDePersonas(CantidadDePersonas),
+  Puntaje is Cantidad / CantidadDePersonas.
+
+totalDePersonas(CantidadDePersonas):-
+  findall(Persona, persona(Persona), TotalDePersonas),
+  length(TotalDePersonas, CantidadDePersonas).
+
+personasSatisfechas(PersonasSatisfechasSinRepetidos):-
+  findall(Persona, (esPropiedad(Propiedad), cumpleTodo(Propiedad, Persona)), PersonasSatisfechasRepetidas),
+  list_to_set(PersonasSatisfechasRepetidas, PersonasSatisfechasSinRepetidos).
+
+propiedadTop(Propiedad):-
+  esPropiedad(Propiedad),
+  not(esChica(Propiedad)),
+  cumpleCondicion(Propiedad, instalaciones([aireAcondicionado])).
+
+esChica(Propiedad) :- tiene(Propiedad, ambientes(1)).
+esChica(Propiedad) :- esPropiedad(Propiedad), not(tiene(Propiedad, ambientes(_))).
+
+cumpleCondicion(Propiedad, instalaciones(InstalacionesDeseadas)) :-
+  esPropiedad(Propiedad),
+  tiene(Propiedad, instalaciones(InstalacionesDeLaPropiedad)),
+  forall(member(Instalacion, InstalacionesDeseadas), member(Instalacion, InstalacionesDeLaPropiedad)).
 
 /*consultas:
 ?- tiene(Propiedad, piscina(30)).
@@ -74,7 +115,7 @@ Propiedad2 = calleFalsa123 ;
 Comodidad = ambientes(2) ;
 Comodidad = piscina(15).
 
-?- cumpleCodicion(Propiedad, ambientes(2)).
+?- cumpleCodicion(_, ambientes(2)).
 Propiedad = tinsmithCircle1774 ;
 Propiedad = avMoreno708 ;
 Propiedad = avSiempreViva742 ;
@@ -83,11 +124,23 @@ Propiedad = calleFalsa123
 ?- tiene(Propiedad, Comodidad), loQueQuiere(pedro, Comodidad).
 false.
 
-?- tiene(avMoreno708, Comodidad), loQueQuiere(_, Comodidad).
+?- cumpleCondicion(avMoreno708, Comodidad).
 Comodidad = jardin ;
+Comodidad = jardin ;
+Comodidad = ambientes(3) ;
+Comodidad = ambientes(2) ;
+Comodidad = ambientes(2) ;
+Comodidad = ambientes(3) ;
+Comodidad = ambientes(2) ;
+Comodidad = ambientes(2) ;
+Comodidad = piscina(15) ;
+Comodidad = piscina(15) ;
+Comodidad = piscina(15) ;
+Comodidad = piscina(15) ;
 
-?-  loQueQuiere(_, Comodidad), not(cumpleCondicion(_, Comodidad)).
-Comodidad = piscina(100)
+?- comodidadIncumplida(Comodidad).
+Comodidad = piscina(100) ;
+Comodidad = piscina(100) ;
 
 consultas de la parte 2
 
@@ -103,5 +156,31 @@ Persona = maria ;
 Propiedad = avMoreno708,
 Persona = pedro ;
 
+?- mejorOpcionVersionConForall(Persona, PropiedadMasBarata).
+Persona = carlos,
+PropiedadMasBarata = tinsmithCircle1774 ;
+Persona = maria,
+PropiedadMasBarata = avMoreno708 ;
+Persona = pedro,
+PropiedadMasBarata = avMoreno708 ;
 
+?- mejorOpcionVersionConNot(Persona, PropiedadMasBarata).
+Persona = carlos,
+PropiedadMasBarata = tinsmithCircle1774 ;
+Persona = maria,
+PropiedadMasBarata = avMoreno708 ;
+Persona = pedro,
+PropiedadMasBarata = avMoreno708 ;
+
+?- efectividad(Puntaje).
+Puntaje = 0.6.
+
+?- propiedadTop(Propiedad).
+Propiedad = tinsmithCircle1774 ;
+Propiedad = avMoreno708
+
+el concepto que utilice para agregar las instalaciones a la base de conocimiento
+fue el polimorfismo, el cual me permitio que en el predicado "cumpleCondicion", que
+es el que utiliza las comodidades de las propiedades, los trate de la misma manera
+a los functores, que son unidades compuestas, de las que no lo son.
 */
